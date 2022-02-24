c53e38f4c6719cbaee50bf1767b39f005d20b4ee
lazoliver <wilian.oliveiractt@gmail.com>
2022-02-24 02:32:40 -0300

    Bonus

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
 
+// Definindo a porta do servidor
+const port = 5000;
+
 // Aula 1 - Console Node.Js
 console.log('Hello World');
 
 // Aula 2 - Respondendo String
 app.get("/", (req, res) => {
     res.send('Hello Express');
+});
+
+app.listen(port, (req, res) => {
+    console.log(`Servidor escutando a porta ${port}`);
 });
