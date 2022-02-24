6fde65d26caf476662ea0a8731d4ce4a3750f36b
lazoliver <wilian.oliveiractt@gmail.com>
2022-02-24 04:10:39 -0300

    aula 6

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+require('dotenv').config();
 
 // Definindo a porta do servidor
 const port = 5000;
@@ -7,10 +8,12 @@ const port = 5000;
 // Aula 1 - Console Node.Js
 console.log('Hello World');
 
-/* Aula 2 - Respondendo String
+/* 
+Aula 2 - Respondendo String
 app.get("/", (req, res) => {
     res.send('Hello Express');
-}); */
+});
+*/
 
 // Aula 3 - Caminho para arquivo HTML
 app.get('/', (req, res) => {
@@ -21,10 +24,21 @@ app.get('/', (req, res) => {
 app.use('/public', express.static(__dirname + '/public'));
 
 // Aula 5 - Respondendo JSON
+/* 
 app.get('/json', (req, res) => {
     res.json({
       "message": "Hello json"
     });
+});
+*/
+
+// Aula 6 - Usar .env
+app.get('/json', (req, res) => {
+    if(process.env.MESSAGE_STYLE === 'uppercase') {
+        res.json("hello json".toLocaleUpperCase());
+    } else {
+        res.json({"message": "Hello json"});
+    }
 });
 
 app.listen(port, (req, res) => {
